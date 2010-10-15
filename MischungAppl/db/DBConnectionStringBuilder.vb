7e74f9799bca72f54c6850a029db020af2b4ec11
Oleg Taranenko <olegtaranenko@gmail.com>
2010-10-15 08:47:05 +0200

    use default protocol

--- MischungAppl/db/DBConnectionStringBuilder.vb
+++ MischungAppl/db/DBConnectionStringBuilder.vb
@@ -26,8 +26,8 @@ Public Class DBConnectionStringBuilder
     Public Function build(ByVal uid, ByVal pwd, ByVal server, ByVal db, ByVal connLifetime, ByVal integratedSec, ByVal persistSecInfo) As String
         Dim connString As String
 		connString = "initial catalog=" & db & ";Connection Lifetime=" & connLifetime & _
-					 ";persist security info=" & persistSecInfo & ";data source=" & server _
-					 & ";Network Library=DBMSSOCN"
+					 ";persist security info=" & persistSecInfo & ";data source=" & server
+					 '& ";Network Library=DBMSSOCN"
 
 
         If (integratedSec) Then
