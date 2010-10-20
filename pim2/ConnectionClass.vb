c1cb2cf4ce3f606f963896ee4207676063317fb4
Oleg Taranenko <olegtaranenko@gmail.com>
2010-10-20 09:10:04 +0200

    connection file for pim

---
+++ pim2/ConnectionClass.vb
@@ -0,0 +1,46 @@
+Public Class ConnectionClass
+
+    Sub New()
+        
+    End Sub
+
+    'connect string 
+    Friend Const ConnectionString As String = "packet size=4096;" & _
+               "integrated security=SSPI;data source=deladb002;persist security info=False;initial catalog=pim;" & _
+               "Connection Lifetime=300"
+
+    Friend SQLConnection As System.Data.SqlClient.SqlConnection
+
+    'get connect to the sql server 
+    Public Function GetSQLConnection() As Boolean
+        Try
+            If SQLConnection Is Nothing Then
+                SQLConnection = New System.Data.SqlClient.SqlConnection(ConnectionString)
+            End If
+
+            If SQLConnection.State = ConnectionState.Closed Then
+                SQLConnection.Open()
+                Return True
+            Else
+                Return True
+            End If
+
+        Catch excp As Exception
+            Return False
+            Exit Function
+        End Try
+
+    End Function
+
+    'close connect to the sql server 
+    Function CloseSQLConnection() As Boolean
+        If Not SQLConnection Is Nothing Then
+            If SQLConnection.State = ConnectionState.Open Then
+                SQLConnection.Close()
+                SQLConnection = Nothing
+                Return True
+            End If
+        End If
+    End Function
+
+End Class
